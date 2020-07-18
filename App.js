a33b7bab935cf504672c2ba3a46757a8b4141d2d
kathansheth04 <50056395+kathansheth04@users.noreply.github.com>
2020-07-18 01:13:48 -0700

    connection to api

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component, useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://webknox.com:8080/recipes/search?query=tomato soup&apiKey=c9f71a6be7174d85ae404ae2a9cb7e39')
+      .then((response) => response.json())
+      .then((json) => setData(json.results))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+          <Text>{item.title}, {item.readyInMinutes}, {item.sourceUrl}</Text>
+          )}
+        />
+      )}
     </View>
   );
 }
