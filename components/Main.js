5a493848155e18c1d0f01c8c3d90ac75a8d2c605
kathansheth04 <50056395+kathansheth04@users.noreply.github.com>
2020-07-18 19:23:57 -0700

    main page refinements

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import {FlatList, Platform, StyleSheet, ActivityIndicator, Text, View, Alert, TextInput } from 'react-native';
-import {Header, Right, Left, Icon} from 'native-base'
-
+import {FlatList, Image, Linking, StyleSheet, ActivityIndicator, Text, View, Dimensions, TextInput } from 'react-native';
+import {Header, Body, Right, Left, Icon} from 'native-base'
+import { Title } from 'react-native-paper';
 
 export default class Main extends Component {
     constructor(props) {
@@ -60,8 +60,10 @@ export default class Main extends Component {
         return (
           <View
             style={{
-              height: 0.3,
-              width: '90%',
+                justifyContent: 'center',
+                alignItems: "center",
+              height: 1,
+              width: '110%',
               backgroundColor: '#080808',
             }}
           />
@@ -71,7 +73,7 @@ export default class Main extends Component {
         if (this.state.isLoading) {
           //Loading View while data is loading
           return (
-            <View style={{ flex: 1, paddingTop: 20 }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
               <ActivityIndicator />
             </View>
           );
@@ -79,36 +81,95 @@ export default class Main extends Component {
         return (
           //ListView to show with textinput used as search bar
           <View style={styles.container}>
+              
               <Header style={{backgroundColor: '#fff'}}>
                   <Left>
-                      <Icon name="search" onPress={() => this.props.navigation.navigate("searchScreen")}/>
+                      {/* nothing */}
                   </Left>
+                  <Body>
+                      <Title>Trending</Title>
+                  </Body>
+                  <Right>
+                      <Icon name="search" onPress={() => this.props.navigation.navigate("searchScreen")}/>
+                  </Right>
+                  
               </Header>
-              <FlatList
-              data={this.state.dataSource}
-              ItemSeparatorComponent={this.ListViewItemSeparator}
-              //Item Separator View
-              renderItem={({ item }) => (
-                // Single Comes here which will be repeatative for the FlatListItems
-              <Text style={styles.textStyle}>{item.title} {'\n'} {'ready in'} {item.readyInMinutes} {'mins'}</Text>
-              )}
-              enableEmptySections={true}
-              style={{ marginTop: 10 }}
-              keyExtractor={(item, index) => index.toString()}
-            />
+                <FlatList
+                data={this.state.dataSource}
+                ItemSeparatorComponent={this.ListViewItemSeparator}
+                //Item Separator View
+                renderItem={({ item }) => (
+                    // Single Comes here which will be repeatative for the FlatListItems
+                    <View style={{ flexDirection: 'row'}}>
+                        <View style={styles.leftContainer}>
+                            <Image style={styles.gridItemImage}source={require('./assets/mainCourse.jpg')}/>
+                        </View>
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.gridItemText1}>{item.title}</Text>
+                        <Text style={styles.gridItemText2}>{item.readyInMinutes} {'mins'}</Text>
+                    </View>
+                    </View>
+                )}
+                enableEmptySections={true}
+                style={{ marginTop: 10 }}
+                keyExtractor={(item, index) => index.toString()}
+                />
           </View>
         );
       }
     }
-     
+    //                        <Text onPress={() => Linking.openURL(item.sourceUrl)} style={styles.gridItemText}>{item.sourceUrl}</Text>
+
+    const {height, width} = Dimensions.get('window');
+
     const styles = StyleSheet.create({
       container: {
         justifyContent: 'center',
         flex: 1,
         backgroundColor:'#fff',
-        marginTop: 0
+        marginTop: 10,
+        paddingBottom: 20,
+        paddingLeft: 10
       },
-      textStyle: {
-        padding: 10,
+      leftContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems:'flex-start',
+        marginRight: 100,
       },
+      rightContainer: {
+        flex: 1,
+        flexWrap: 'wrap',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+        gridItemImage: {
+            justifyContent: 'flex-start',
+            alignSelf: 'flex-start',
+            paddingTop: 100,
+            width: 100,
+            height: 100,
+            borderWidth: 1.5, 
+            borderColor: 'white',
+            borderRadius: 20,
+        },
+        gridItemText1: {
+            flexDirection: 'row',
+            flex: 1,
+            fontSize: 20,
+            marginTop: 5,
+            alignItems: 'flex-start',
+            justifyContent: 'center'
+        },
+        gridItemText2: {
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            flex: 1,
+            fontSize: 16,
+            marginTop: 5,
+            alignItems: 'flex-start',
+            justifyContent: 'center'
+        },
     });
